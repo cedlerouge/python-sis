29f3d57c608c9e8ecaddb142a01dcb3d7f1bc026
Cédric ROGER <4787982+cedlerouge@users.noreply.github.com>
2022-09-14 22:22:43 +1100

    add: search in page

--- sis.py
+++ sis.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python 
 
 #import json
+from logging import root
 import requests
 #from bs4 import BeautifulSoup
+import re
 
 _config_filename = 'settings.json'
 
-_website = "https://betwit.lepont.bzh"
+_website = "https://example.com"
 _sitemap_uri = "/sitemap.xml"
-_search_string = "dupont" 
+_search_string = "lorem" 
 
 
 if __name__ == '__main__':
@@ -18,8 +20,27 @@ if __name__ == '__main__':
         r = session.get(_website + _sitemap_uri)
         print(r.text)
 
-    # scrape earche URI 
+        #from lxml import etree
+        #tree = etree.parse
+        import xml.etree.ElementTree as ET
+        root = ET.fromstring(r.text)
+        #print(tree[0].tag)
 
+        urlList = []
+        for sitemap in root:
+            if sitemap.getchildren():
+                children = sitemap.getchildren()
+                urlList.append(children[0].text)
+        print(urlList)
+            #for x in elt:
+            #    print(x.tag, x.attrib, x.getchildren())
+            #    print(dir(x))
 
+        loremList = []
+        # scrape eache URI
+        for url in urlList:
+            r = session.get(url)
+            if re.search(_search_string, r.text, re.IGNORECASE):
+                loremList.append(url)
 
-    # list sucessful search pages
+        print(loremList)
